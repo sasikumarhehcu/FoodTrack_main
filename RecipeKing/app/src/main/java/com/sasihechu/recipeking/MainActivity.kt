ce524f3656822bc2d3577c62a2de8769d0e597bd
sasikumarhehcu <hehchusasikunar@gmail.com>
2025-05-29 17:09:52 +1000

    mobiletechnology

--- RecipeKing/app/src/main/java/com/sasihechu/recipeking/MainActivity.kt
+++ RecipeKing/app/src/main/java/com/sasihechu/recipeking/MainActivity.kt
@@ -4,23 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sasihechu.recipeking.ui.theme.RecipeKingTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             RecipeKingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    TestLayout(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +42,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TestLayout(name: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(modifier = Modifier.fillMaxHeight().width(100.dp).background(Color.Yellow),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Column 1")
+        }
+        Column(modifier = Modifier.fillMaxHeight().width(100.dp).background(Color.Red),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Column 2")
+        }
+        Column(
+            modifier = Modifier.fillMaxHeight().fillMaxWidth().background(Color.Gray),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(text = "Column 3")
+        }
+    }
 }
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TestLayout() {
     RecipeKingTheme {
-        Greeting("Android")
+        TestLayout("Android")
     }
 }
