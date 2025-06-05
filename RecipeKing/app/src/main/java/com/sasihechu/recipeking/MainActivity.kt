8d0a5ba8cea217aca15f600e711d72979d34ae5c
sasikumarhehcu <hehchusasikunar@gmail.com>
2025-06-05 14:37:47 +1000

    mobiletechnology

--- RecipeKing/app/src/main/java/com/sasihechu/recipeking/MainActivity.kt
+++ RecipeKing/app/src/main/java/com/sasihechu/recipeking/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.sasihechu.recipeking
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,15 +21,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sasihechu.recipeking.ui.theme.RecipeKingTheme
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContent {
             RecipeKingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -41,33 +43,65 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+private val image_ids = listOf(
+    R.drawable.dice_1,
+    R.drawable.dice_2,
+    R.drawable.dice_3,
+    R.drawable.dice_4,
+    R.drawable.dice_5,
+    R.drawable.dice_6
+)
+
 @Composable
 fun TestLayout(name: String, modifier: Modifier = Modifier) {
     Row(
         modifier = Modifier.fillMaxSize()
     ) {
-        Column(modifier = Modifier.fillMaxHeight().width(100.dp).background(Color.Yellow),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = "Column 1")
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(100.dp)
+                .background(Color.Yellow),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val context = LocalContext.current
+            repeat(times = 6) { index ->
+                Image(
+                    painter = painterResource(image_ids[index]),
+                    contentDescription = "Dice ${index + 1}",
+                    modifier = Modifier.clickable {
+                        Toast.makeText(context, "You clicked me ${index + 1}", Toast.LENGTH_SHORT).show()
+                    }
+                )
+            }
         }
-        Column(modifier = Modifier.fillMaxHeight().width(100.dp).background(Color.Red),
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(100.dp)
+                .background(Color.Blue),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally) {
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(text = "Column 2")
         }
         Column(
-            modifier = Modifier.fillMaxHeight().fillMaxWidth().background(Color.Gray),
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .background(Color.Green),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
+        ) {
             Text(text = "Column 3")
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
-fun TestLayout() {
+fun TesPreview() {
     RecipeKingTheme {
         TestLayout("Android")
     }
